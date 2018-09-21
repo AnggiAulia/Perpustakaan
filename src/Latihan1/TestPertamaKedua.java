9d97c432b9f7a762ec9a2c3667228cbc7ee0097f
ASUS <ASUS@LAPTOP-29V2RE2F>
2018-09-21 11:45:19 +0700

    FINAL

---
+++ src/Latihan1/TestPertamaKedua.java
@@ -0,0 +1,15 @@
+
+package Latihan1;
+
+public class TestPertamaKedua {
+      public static void main(String[] arg){
+      Kedua D2 = new Kedua ();
+      D2.BacaSuper ();
+      D2.info();
+      
+      Pertama S1 = new Pertama ();
+      S1.terprotek ();
+      S1.info ();
+      }
+    
+}
---
+++ src/Latihan3/Gajah.java
@@ -0,0 +1,21 @@
+
+package Latihan3;
+
+public class Gajah extends Hewan {
+    public static void testClassMethod()
+    {
+    System.out.println("The class method in Hewan...");
+    }
+ //meng-ovveride method pada class Animal
+public void testInstanceMethod()
+{
+    System.out.println("the instance method in Gajah..");
+}
+public static void main (String[]args)
+{
+    Gajah myGajah = new Gajah();
+    Hewan myHewan = myGajah;
+    Hewan.testClassMethod();
+    myHewan.testInstanceMethod();
+}
+}
